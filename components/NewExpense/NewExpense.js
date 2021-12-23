abc94f1824c5415c13c5dd1d381cdbb0ef371cd4
RHKarthick <95705321+RHKarthick@users.noreply.github.com>
2021-12-22 21:28:55 -0800

    Add files via upload

---
+++ components/NewExpense/NewExpense.js
@@ -0,0 +1,18 @@
+import react from "react";
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm';
+function NewExpense(props){
+    const saveExpensedataHandler = (enteredExpenseData)=>{
+       const expenseData={
+           ...enteredExpenseData,
+           id:Math.random().toString()
+       };
+       props.onAddExpense(expenseData);
+    };
+    return(
+        <div className="new-expense">
+         <ExpenseForm onSaveExpenseData={saveExpensedataHandler}/>   
+        </div>
+    );
+}
+export default NewExpense;
