d0f31460beb9319b53d071e89eec9ac00b833149
RHKarthick <95705321+RHKarthick@users.noreply.github.com>
2021-12-22 21:30:41 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "camera",
+    amount: 19940,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 77799, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 29400,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 4500,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+const [expenses,setExpenses]=useState(DUMMY_EXPENSES);  
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses)=>{return[expense,...prevExpenses];});
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
